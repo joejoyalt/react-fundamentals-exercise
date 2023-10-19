efbb9f4cf041ef0e90f065a5c710f4b65cf568a1
Nerijus Muranovas <nerijus.muranovas@vinted.com>
2023-10-19 12:00:29 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="center-text">2023 React Fundamentals Workshop</h1>
+      <div>Add code here</div>
+    </div>
+  );
+}
+
+export default App;
